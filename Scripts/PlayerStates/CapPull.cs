b9a7dc35daae68b477546cea5b60ea75c0850e13
kruumy <joshuampauls@gmail.com>
2025-02-22 20:41:12 -0500

    Add minimum distance check for CapPull state

--- Scripts/PlayerStates/CapPull.cs
+++ Scripts/PlayerStates/CapPull.cs
@@ -5,13 +5,21 @@ namespace Scripts.PlayerState
     public class CapPull : State
     {
         public const float CapPullVelocity = 20f;
+        public const float MinDistanceToCap = 2f;
         protected override void UpdateVelocity(ref Vector3 newVelocity, float delta)
         {
             if(Player.Cap.IsThrown)
             {
                 Player.CoolDowns.Remove("GroundPoundFreeze");
                 Vector3 directionToCap = Player.Cap.GlobalPosition - Player.GlobalPosition;
-                newVelocity = directionToCap.Normalized() * Mathf.Max(newVelocity.Length(), CapPullVelocity) ;
+                if (directionToCap.Length() > MinDistanceToCap)
+                {
+                    newVelocity = directionToCap.Normalized() * Mathf.Max(newVelocity.Length(), CapPullVelocity);
+                }
+                else
+                {
+                    Player.CurrentState = new Idle();
+                }
             }
             else
             {
